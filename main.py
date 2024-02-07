4064061e253458fb773b014bf9c94dd7d8086300
Dmitriy <dim1a15@mail.ru>
2024-02-07 12:31:05 +0300

    Добавление функции git_info_files

--- main.py
+++ main.py
@@ -5,17 +5,31 @@ API_KEY = "ff6414e7cf4130ce0fcaa0e9e12d83962039bbfa715277100ff5fe7023274f04"
 
 def upload_file(path):
     url = "https://www.virustotal.com/api/v3/files"
-    files = { "file": ("virus2", open("virus2", "rb"), "application/octet-stream") }
+
+    files = { "file": (path, open(path, "rb"))}
     headers = {
         "accept": "application/json",
-        "x-apikey": API_KEY,
-        "content-type": "multipart/form-data"
+        "x-apikey": API_KEY
     }
 
     response = requests.post(url, files=files, headers=headers)
 
     return response.json()["data"]["links"]["self"]
 
-result = upload_file("virus2")
-print(result)
 
+def git_info_files(path):
+
+    url = upload_file(path)
+
+    headers = {
+        "accept": "application/json",
+        "x-apikey": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+
+result = git_info_files("text.txt")
+
+print(result)
