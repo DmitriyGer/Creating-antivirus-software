9f99c1ba5b2bf4d438680f97f35ae07f8735eb68
JohnHinster <slava_dremin@mail.ru>
2024-02-07 12:52:48 +0300

    ачало работы с дизайном

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-import requests
-
-API_KEY = "ff6414e7cf4130ce0fcaa0e9e12d83962039bbfa715277100ff5fe7023274f04"
-
-
-def upload_file(path):
-    url = "https://www.virustotal.com/api/v3/files"
-    files = { "file": ("virus2", open("virus2", "rb"), "application/octet-stream") }
-    headers = {
-        "accept": "application/json",
-        "x-apikey": API_KEY,
-        "content-type": "multipart/form-data"
-    }
-
-    response = requests.post(url, files=files, headers=headers)
-
-    return response.json()["data"]["links"]["self"]
-
-result = upload_file("virus2")
-print(result)
-
+from tkinter import *  
+from tkinter import messagebox  
+  
+  
+def clicked():  
+    messagebox.showinfo('Заголовок', 'Текст')  
+  
+  
+window = Tk()  
+window.title("Добро пожаловать в приложение PythonRu")  
+window.geometry('400x250')  
+btn = Button(window, text='Клик', command=clicked)  
+btn.grid(column=0, row=0)  
+window.mainloop()
