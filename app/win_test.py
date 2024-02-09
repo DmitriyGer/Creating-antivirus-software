9306e8c4348df10376347625642a918bb83f5fa5
Dmitriy <dim1a15@mail.ru>
2024-02-09 22:44:05 +0300

    Готовая версия файла win_test.py #1

--- app/win_test.py
+++ app/win_test.py
@@ -13,21 +13,22 @@ class TestWin(QWidget):
         self.initUI()
         self.show()
 
+    """ Настройка экрана """
     def set_win(self):
-        """ Настройка экрана """
         self.setWindowTitle("BaohuMe - Окно сканирования")
         self.resize(700, 500)
     
+    """ Создание виджета и направляющих """
     def initUI(self):
-        """ Создание виджета и направляющих """
         layout = QVBoxLayout()
-
         s_a = QScrollArea(self)
 
+
+        """ Объявление базы данных """
         data = database.git_info_files(self.cur_file)
 
 
-        """ Вывод всех антивирусов и их результатов сканирования списком """
+        # """ Построчный перебор по базе данных и вывод результатов сканирования """
         # if len(data) == 0:
         #     label = QLabel("Запрос создан, ожидайте ...")
         #     layout.addWidget(label, alignment=Qt.AlignCenter)
@@ -37,44 +38,32 @@ class TestWin(QWidget):
         #         label = QLabel(result)
         #         layout.addWidget(label, alignment=Qt.AlignCenter)
 
+
         """ Название сканируемого файла """
         name_file = self.cur_file.split("/")[-1]
 
+
+        """ Перебор по БД обнаружение кол-во срабатываний """
         return_scan = 0
         for name in data:
             if data[name]['result'] != None:
                 return_scan += 1
 
+
+        """ Вывод результатов сканирования пользователю """
         if len(data) == 0:
             label = QLabel("Запрос создан, ожидайте ...")
             layout.addWidget(label, alignment=Qt.AlignCenter)
         else:
             if return_scan == 0:
-                print("угроз не обнаружено")
-                result = f"В файле {name_file} угроз не обнаружено"
+                result = f"В файле {name_file} угрозы не обнаружены"
                 label = QLabel(result)
                 layout.addWidget(label, alignment=Qt.AlignCenter)
             else:
-                print("угроз обнаружено")
-                result = f"В файле {name_file} обнаружена"
+                result = f"В файле {name_file} обнаружена угроза"
                 label = QLabel(result)
                 layout.addWidget(label, alignment=Qt.AlignCenter)
-
-
-            # for name in data:
-            #     if data[name]['result'] == None:
-            #         print("угроз не обнаружено")
-            #         result = f"В файле {name_file} угроз не обнаружено"
-            #         label = QLabel(result)
-            #         layout.addWidget(label, alignment=Qt.AlignCenter)
-            #     else:
-            #         print("угроз обнаружено")
-            #         result = f"В файле {name_file} обнаружена"
-            #         label = QLabel(result)
-            #         layout.addWidget(label, alignment=Qt.AlignCenter)
                 
-
-
 
         """ Проверка работы вывода алгоритма. Простой список от 1 до 100 """
         # for i in range(100):
@@ -88,7 +77,3 @@ class TestWin(QWidget):
 
         main_layout = QVBoxLayout(self)
         main_layout.addWidget(s_a)
-
-
-
-    
