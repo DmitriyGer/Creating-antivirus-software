2af08d790493d7227f7e7ca379e37a9ed077311e
Dmitriy <dim1a15@mail.ru>
2024-02-10 00:00:21 +0300

    Написание окна для удаления или игнор угрозы #1

---
+++ app/del_malwer.py
@@ -0,0 +1,47 @@
+import os
+from PyQt5.QtWidgets import (
+    QWidget, QPushButton, QVBoxLayout, QLabel
+)
+from PyQt5.QtCore import Qt
+
+class WarningWindow(QWidget):
+    def __init__(self, cur_file):
+        super().__init__()
+        self.cur_file = cur_file
+        self.set_win()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_win(self):
+        '''Нвстройка экрана'''
+        self.setWindowTitle('BaohuMe - Внимание, угроза!')
+        self.resize(400, 200)
+
+
+    """ Создание виджета и направляющих """
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        """ Работа над кнопками "Удалить" или "Игнорировать" """
+        self.delite = QPushButton('Удалить')
+        self.ignore = QPushButton('Игнорировать')
+
+        layout.addWidget(self.delite, alignment = Qt.AlignLeft)
+        layout.addWidget(self.ignore, alignment = Qt.AlignRight)
+
+        self.setLayout(layout)
+
+
+    """ Подключение событий """
+    def connects(self):
+        self.delite.clicked.connect(self.click_delite)
+
+    
+    """ Функция удаления файла """
+    def click_delite(self):
+        layout = QVBoxLayout()
+        os.remove(self.cur_file)
+        result = "Угроза успешно удалена"
+        label = QLabel(result)
+        layout.addWidget(label, alignment=Qt.AlignCenter)
