9f8127510d79769cdc4b08224cfcb23e77be38dd
Dmitriy <dim1a15@mail.ru>
2024-02-09 22:15:37 +0300

    Вывод сообщения об обнаружении угрозы и имя файла в котором она не/обнаружена

--- app/win_test.py
+++ app/win_test.py
@@ -26,15 +26,57 @@ class TestWin(QWidget):
 
         data = database.git_info_files(self.cur_file)
 
+
+        """ Вывод всех антивирусов и их результатов сканирования списком """
+        # if len(data) == 0:
+        #     label = QLabel("Запрос создан, ожидайте ...")
+        #     layout.addWidget(label, alignment=Qt.AlignCenter)
+        # else:
+        #     for name in data:
+        #         result = f"Антивирус: {name}\n Результат: {data[name]['result']}\n"
+        #         label = QLabel(result)
+        #         layout.addWidget(label, alignment=Qt.AlignCenter)
+
+        """ Название сканируемого файла """
+        name_file = self.cur_file.split("/")[-1]
+
+        return_scan = 0
+        for name in data:
+            if data[name]['result'] != None:
+                return_scan += 1
+
         if len(data) == 0:
             label = QLabel("Запрос создан, ожидайте ...")
             layout.addWidget(label, alignment=Qt.AlignCenter)
         else:
-            for name in data:
-                result = f"Антивирус: {name}\n Результат: {data[name]['result']}\n"
+            if return_scan == 0:
+                print("угроз не обнаружено")
+                result = f"В файле {name_file} угроз не обнаружено"
+                label = QLabel(result)
+                layout.addWidget(label, alignment=Qt.AlignCenter)
+            else:
+                print("угроз обнаружено")
+                result = f"В файле {name_file} обнаружена"
                 label = QLabel(result)
                 layout.addWidget(label, alignment=Qt.AlignCenter)
 
+
+            # for name in data:
+            #     if data[name]['result'] == None:
+            #         print("угроз не обнаружено")
+            #         result = f"В файле {name_file} угроз не обнаружено"
+            #         label = QLabel(result)
+            #         layout.addWidget(label, alignment=Qt.AlignCenter)
+            #     else:
+            #         print("угроз обнаружено")
+            #         result = f"В файле {name_file} обнаружена"
+            #         label = QLabel(result)
+            #         layout.addWidget(label, alignment=Qt.AlignCenter)
+                
+
+
+
+        """ Проверка работы вывода алгоритма. Простой список от 1 до 100 """
         # for i in range(100):
         #     label = QLabel(f"Строка номер {i}")
         #     layout.addWidget(label, alignment=Qt.AlignCenter) 
