6c3f41fc62da463d5c91a93b57f7ea4d960cefbf
Dmitriy <dim1a15@mail.ru>
2024-02-20 15:42:35 +0300

    Рабочая версия антивируса №1

--- antivirusSoftware.py
+++ antivirusSoftware.py
@@ -1,31 +1,71 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QStackedWidget, QMessageBox, QFileDialog
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QStackedWidget, QMessageBox, QFileDialog, QScrollArea
+
+from helpers import database
+from app import del_malwer
 
 """ Страница сканирования"""
 class ScanPage(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        """Текст в блоке 'Страница' """
         self.layout = QVBoxLayout()
         self.label = QLabel("Страница сканирования\n")
         self.layout.addWidget(self.label)
-
+       
+        """ Кнопка 'Выбора файла' """
         self.file_button = QPushButton("Выбрать файл")
         self.file_button.clicked.connect(self.select_file)
         self.layout.addWidget(self.file_button)
 
         self.setLayout(self.layout)
-
-    # Функции
         
-    # Функция выбора файла
+
+    """ Функция выбора файла """
     def select_file(self):
         options = QFileDialog.Options()
         # options |= QFileDialog.ReadOnly
         file, _ = QFileDialog.getOpenFileName(self, "Выберите файл для сканирования", "", "Все файлы (*)", options=options)
         if file:
-            print("Выбран файл:", file)
+            print("Выбран файл:", file)   
+            self.show_testing_page(file)         
+            
+
+    """ Функция вызова стр. тест """
+    def show_testing_page(self, file):
+        data = database.git_info_files(file)
+        name_file = file.split("/")[-1]
+        # self.label.setText(f"Выбранный файл: {name_file}")
+        
+        """ Перебор по БД, обнаружение кол-во срабатываний """
+        return_scan = 0
+        for name in data:
+            if data[name]['result'] != None:
+                return_scan += 1
+
+        """ Вывод результатов сканирования пользователю """
+        if len(data) == 0:
+            self.label.setText("Запрос создан, ожидайте ...")
+            print("Запрос создан")
+        else:
+            if return_scan == 0:
+                self.label.setText(f"В файле {name_file} угрозы не обнаружены")
+                print("Нет")
+            else:
+                print("Усть")
+                self.label.setText(f"В файле {name_file} обнаружена угроза")
+                # self.dm = del_malwer.WarningWindow(file)
+                del_malwer(file)
+
+    """ Функция удаления файла """
+    def del_malwer(self, file):
+        confirm = QMessageBox.question(self, 'BaohuMe - Подтверждение', 'Вы действительно хотите удалить файл?', QMessageBox.Yes | QMessageBox.No)
+        if confirm == QMessageBox.Yes:
+            os.remove(file)
+            print("Удален успешно")
+
 
 """ Страница о разработчиках """
 class DevelopersPage(QWidget):
@@ -38,23 +78,13 @@ class DevelopersPage(QWidget):
 
         self.setLayout(self.layout)
 
-""" Страница тестирования """
-class TestPage(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.layout = QVBoxLayout()
-        self.label = QLabel("Страница тестирования")
-        self.layout.addWidget(self.label)
-
-        self.setLayout(self.layout)
 
 """ Основной класс """
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('BaohuMe')
-        self.resize(600, 400)
+        self.resize(800, 500)
 
         self.menu_layout = QVBoxLayout()
         self.page_widget = QStackedWidget()
@@ -86,9 +116,10 @@ class MainWindow(QWidget):
     def load_page(self, page_num):
         self.page_widget.setCurrentIndex(page_num)
 
+
+    """ Закрытие программы """
     def close_app(self):
         confirm = QMessageBox.question(self, 'Подтверждение', 'Вы уверены, что хотите закрыть?', QMessageBox.Yes | QMessageBox.No)
-        
         if confirm == QMessageBox.Yes:
             self.close()
 
