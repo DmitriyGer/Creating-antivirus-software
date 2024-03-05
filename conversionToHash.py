bda87a69824444ed325e08efcb6e3354327a2d2d
Dmitriy <dim1a15@mail.ru>
2024-03-05 15:33:18 +0300

    написание файла для хеширования файла

---
+++ conversionToHash.py
@@ -0,0 +1,23 @@
+""" Перевод файла в хеш код НУЖНО """
+import hashlib
+
+def calculate_file_hash(file_path):
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+def check_file_integrity(file_path, expected_hash):
+    file_hash = calculate_file_hash(file_path)
+    if file_hash == expected_hash:
+        print(f"The file {file_path} is intact.")
+    else:
+        print(f"The file {file_path} has been modified.")
+        print(f"Expected hash: {expected_hash}")
+        print(f"Actual hash: {file_hash}")
+
+file_path = "file1.exe"
+expected_hash = "your_expected_hash"
+
+check_file_integrity(file_path, expected_hash)
