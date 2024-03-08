97796f079e4952960f994338473d55f862b1861a
Dmitriy <dim1a15@mail.ru>
2024-03-08 23:15:58 +0300

    Работа с вкладкой сканирование в реальном времени, удаление не нужного

--- antivirusSoftware.py
+++ antivirusSoftware.py
@@ -105,9 +105,6 @@ class ScanRealTime(QWidget):
             self.timer.timeout.connect(self.check_for_new_file)
             self.timer.start(2000)  # Проверять наличие нового файла каждую секунду
 
-
-    
-
     """ Функция для обнаружения новых файлов в папке и проверки на наличие угроз при помощи хеширования """       
     def check_for_new_file(self):
         
@@ -120,176 +117,16 @@ class ScanRealTime(QWidget):
                             with open(file_path, "rb") as f:
                                 file_hash = hashlib.sha256(f.read()).hexdigest()
                                 if file_hash == target_hash:
-                                    print(f"Файл с хешем {target_hash} найден в папке: {root}")
+                                    print(f"В папке {root} обнаружена угроза: {file_name}")
+                                    # print(f"Обнаружена угроза в файле {file_name} с хешем {target_hash} найден в папке: {root}")
                                     break
                     break
         # Пример использования функции
         username = os.getlogin()
-        target_hashes = ["16355db04c8444072383393139fff3f6e6c467e475710a29d5182daebede711c", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"]
+        target_hashes = ["e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", "16355db04c8444072383393139fff3f6e6c467e475710a29d5182daebede711c"]
         folders = [f"C:\\Users\\{username}\\Desktop\\TestFolder", f"C:\\Users\\{username}\\Downloads", f"C:\\Users\\{username}\\Desktop", f"C:\\Users\\{username}\\Documents"]
         find_files_by_hashes(target_hashes, folders)
-
-
-
-        # # Функция для поиска
-        # def search_file(file_name, directories):
-        #     results = []
-        #     for directory in directories:
-        #         for root, dirs, files in os.walk(directory):
-        #             if file_name in files:
-        #                 results.append(os.path.join(root, file_name))
-        #     return results
         
-        # Создаем базу данных с хешами безопасных файлов
-        safe_files = {
-            "file1": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-            "file2.exe": "z9y8x7w6v5u4t3s2r1",
-            # Добавьте сюда остальные файлы
-        }
-        # search_file_len = len(safe_files)
-
-        # # Укажите имя файла, который хотите найти
-        # file_to_find = "virus2"
-
-        # # Укажите список папок, в которых нужно выполнить поиск
-        # username = os.getlogin()
-        # directories_to_search = [f"C:\\Users\\{username}\\Desktop\\TestFolder", f"C:\\Users\\{username}\\Downloads", f"C:\\Users\\{username}\\Desktop", f"C:\\Users\\{username}\\Documents"]
-        # directories_to_search_len = len(directories_to_search)
-        
-        # for i in range(directories_to_search_len):
-        #     directories_to_search_str = directories_to_search[i]
-        #     print(directories_to_search_str)
-        
-        # with open(directories_to_search_str + "\\" + file_to_find, "rb") as file:
-        #         content = file.read()
-        
-
-        # # Хешируем содержимое файла
-        # hash_obj = hashlib.sha256()
-        # hash_obj.update(content)
-        # file_hash = hash_obj.hexdigest()
-
-
-        # # Проверяем, есть ли хеш в базе безопасных файлов
-        # if file_hash in safe_files.values():
-        #     print(f"В файле {file_to_find} обнаружена угроза, она находится в папке {directories_to_search}")
-        # else:
-        #     print(f"В файле {file_to_find} угроз не обнаружено")
-
-
-
-
-
-        # found_files = search_file(file_to_find, directories_to_search)
-
-        # if found_files:
-        #     # print(f"Найден файл '{file_to_find}' в следующих папках:")
-        #     for file_path in found_files:
-        #         path_file = file_path
-        #         # print(file_path)
-        # # else:
-        # #     print(f"Файл '{file_to_find}' не найден в указанных папках.")
-
-        # # with open(directories_to_search_str + "\\" + file_to_find, "rb") as file:
-        # #     content = file.read()
-
-        # # Хешируем содержимое файла
-        # hash_obj = hashlib.sha256()
-        # hash_obj.update(content)
-        # file_hash = hash_obj.hexdigest()
-
-
-        # # Проверяем, есть ли хеш в базе безопасных файлов
-        # if file_hash in safe_files.values():
-        #     print(f"В файле {file_to_find} обнаружена угроза, она находится в папке {path_file}")
-        # else:
-        #     print(f"В файле {file_to_find} угроз не обнаружено")
-
-
-
-
-
-
-        # # Создаем базу данных с хешами безопасных файлов
-        # safe_files = {
-        #     "file1": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-        #     "file2.exe": "z9y8x7w6v5u4t3s2r1",
-        #     # Добавьте сюда остальные файлы
-        # }
-        
-        # # Путь к папаке для отслеживания новых файлов
-        # username = os.getlogin()
-        # folder_path = f"C:\\Users\\{username}\\Desktop\\TestFolder"
-
-        # # Последний добавленный файл
-        # last_added_file = ''
-
-        # # Прогонка по папке
-        # files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        # files.sort(key=lambda x: os.path.getctime(os.path.join(folder_path, x)), reverse=True)
-        # print(files)
-
-        # # Прогонка по папкам
-        # for folders in folder_path:
-        #     for root, dirs, files in os.walk(folders):
-        #         for file in files:
-        #             file_path = os.path.join(root, file)
-        #             # print(file_path)
-                    
-        # # Выбираем из всех файлов последний
-        # if files:
-        #     latest_file = files[0]
-        #     if latest_file != last_added_file:
-        #         last_added_file = latest_file
-        
-        # # Простматриваем файл с проводника
-        # with open(folder_path + "\\" + last_added_file, "rb") as file:
-        #     content = file.read()
-
-        # # Хешируем содержимое файла
-        # hash_obj = hashlib.sha256()
-        # hash_obj.update(content)
-        # file_hash = hash_obj.hexdigest()
-
-        # # Проверяем, есть ли хеш в базе безопасных файлов
-        # if file_hash in safe_files.values():
-        #     print(f"В файле {last_added_file} обнаружена угроза")
-        # else:
-        #     print(f"В файле {last_added_file} угроз не обнаружено")
-
-            
-        
-        """ В скором времени удалить """
-        # if confirm == QMessageBox.Yes:
-        # print('Start_real_time_scan')
-        # # Считывание путей к папкам из файла Folder.txt
-        # folder_paths = []
-        # with open('helpers\database_local.txt', 'r') as file:
-        #     for line in file:
-        #         folder_paths.append(line.strip())
-
-        # # Словарь для хранения списка файлов в каждой папке на старте
-        # initial_files = {folder_path: os.listdir(folder_path) for folder_path in folder_paths}
-
-        # # Основной цикл программы
-        # 
-        # while True:
-        #     for folder_path in folder_paths:
-        #         current_files = os.listdir(folder_path)
-
-        #         # Поиск новых файлов
-        #         new_files = [file for file in current_files if file not in initial_files[folder_path]]
-
-        #         # Вывод сообщения при обнаружении нового файла
-        #         for new_file in new_files:
-        #             print(f"Новый файл обнаружен в папке {folder_path}: {new_file}")
-
-        #         initial_files[folder_path] = current_files
-
-        #     # time.sleep(5)  # Проверка каждые 5 секунд
-            
-
-
 
 """ Страница о разработчиках """
 class DevelopersPage(QWidget):
